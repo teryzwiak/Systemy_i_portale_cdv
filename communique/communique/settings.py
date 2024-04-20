06a65cede61d222f3daa6be33aa9faa88aa3cd7c
teryzwiak <patryk.jozwiak2001@gmail.com>
2024-04-20 17:31:13 +0200

    second commit

--- communique/communique/settings.py
+++ communique/communique/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,10 @@ WSGI_APPLICATION = 'communique.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testbaza',
-        'USER': 'postgres',
-        'PASSWORD': 'Minecraft@2001',
-        'HOST': 'localhost',
-        'PORT': '',
+       'NAME': os.environ.get('DBNAME'),
+        'HOST': os.environ.get('DBHOST'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS')
     }
 }
 
